eff9333c00d84c64a9822c1eae489c7b8df7c842
maxcarter <max4carter@gmail.com>
2016-02-13 14:53:59 -0500

    Various UI updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,24 +8,42 @@
  * Controller of the twitterSearchApp
  */
 angular.module('twitterSearchApp')
-    .controller('MainCtrl', function($scope, Twitter, Query) {
+    .controller('MainCtrl', function($scope, $alert, Twitter, Query) {
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
-
         $scope.queryFactory = Query;
         $scope.query = Query.param.get("q");
+        $scope.lang = (Query.param.get("lang")) ? Query.param.get("q") : 'en';
 
         if ($scope.query !== null && $scope.query !== undefined && $scope.query !== '') {
+            $scope.loading = true;
+            $scope.welcome = false;
+
             Twitter.search({
                 q: $scope.query,
-                result_type: "recent"
+                result_type: "recent",
+                lang: $scope.lang
             }).then(
                 function success(response) {
                     $scope.tweets = response.data.statuses;
+
+                },
+                function error(response) {
+                    $scope.loading = false;
+                    $alert({
+                        title: response.status + " " + response.statusText,
+                        content: response.data,
+                        placement: 'top',
+                        type: 'danger',
+                        show: true,
+                        container: '#alert-container'
+                    });
                 });
+        } else {
+            $scope.welcome = true;
         }
     });
