d0dc662b72d806d0efce3a60c26010adb91c16e1
maxcarter <max4carter@gmail.com>
2016-03-13 03:48:27 -0400

    Moved bearer token request into api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var qs = require('qs');
-var https = require('https');
 var cfenv = require("cfenv");
 
 var config = require('./config');
@@ -22,54 +20,7 @@ app.use(express.static('./dist'));
 app.use('/dev', express.static('./app'));
 
 
-var enc_secret = new Buffer(config.consumer_key + ':' + config.consumer_secret).toString('base64');
-var post_data = qs.stringify({
-    'grant_type': 'client_credentials'
-});
-var oauthOptions = {
-    host: 'api.twitter.com',
-    path: '/oauth2/token',
-    method: 'POST',
-    headers: {
-        'Authorization': 'Basic ' + enc_secret,
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Content-Length': Buffer.byteLength(post_data)
-    }
-};
-
-var setBearer = function(token) {
-    console.log("The server is now ready to accept requests.");
-    console.log("Bearer Token: " + token.access_token);
-    config.bearer = token.access_token;
-};
-
-var bearerReq = https.request(oauthOptions, function(response) {
-    var data = '';
-    var error = '';
-    response.on("data", function(d) {
-        data += d;
-    });
-    response.on("error", function(e) {
-        error = e;
-    });
-    response.on('end', function() {
-        if (error) {
-            throw error;
-        } else {
-            setBearer(JSON.parse(data));
-        }
-
-    });
-});
-bearerReq.on("error", function(e) {
-    console.log("Error retrieving the Twitter API Bearer token!");
-    console.log(JSON.stringify(e));
-});
-bearerReq.write(post_data);
-bearerReq.end();
-
-
-require('./api')(app, config, https, qs);
+require('./api')(app);
 
 app.listen(port, host);
 console.log("Started Node.js server " + host + ":" + port);
