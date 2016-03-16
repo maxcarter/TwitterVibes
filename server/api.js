00a0184abccae4f6146bf79311655c27b3e1c58d
maxcarter <max4carter@gmail.com>
2016-03-16 01:50:35 -0400

    Refactored bearer token into controller

--- server/api.js
+++ server/api.js
@@ -3,52 +3,9 @@ var qs = require('qs');
 var https = require('https');
 var Tweet = require('./models/tweet');
 var config = require('./config');
+var twitter = require('./controllers/twitter');
 
-var enc_secret = new Buffer(config.consumer_key + ':' + config.consumer_secret).toString('base64');
-var post_data = qs.stringify({
-    'grant_type': 'client_credentials'
-});
-var oauthOptions = {
-    host: 'api.twitter.com',
-    path: '/oauth2/token',
-    method: 'POST',
-    headers: {
-        'Authorization': 'Basic ' + enc_secret,
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Content-Length': Buffer.byteLength(post_data)
-    }
-};
-
-var setBearer = function(token) {
-    console.log("The server is now ready to accept requests.");
-    console.log("Bearer Token: " + token.access_token);
-    config.bearer = token.access_token;
-};
-
-var bearerReq = https.request(oauthOptions, function(response) {
-    var data = '';
-    var error = '';
-    response.on("data", function(d) {
-        data += d;
-    });
-    response.on("error", function(e) {
-        error = e;
-    });
-    response.on('end', function() {
-        if (error) {
-            throw error;
-        } else {
-            setBearer(JSON.parse(data));
-        }
-
-    });
-});
-bearerReq.on("error", function(e) {
-    console.log("Error retrieving the Twitter API Bearer token!");
-    console.log(JSON.stringify(e));
-});
-bearerReq.write(post_data);
-bearerReq.end();
+twitter.get.bearer();
 
 var analyze = function(data) {
     var summary = {
---
+++ server/controllers/twitter.js
@@ -0,0 +1,56 @@
+var https = require('https');
+var qs = require('qs');
+var config = require('../config');
+
+var callback = {
+    set: {
+        bearer: function(token) {
+            console.log("The server is now ready to accept requests.");
+            console.log("Bearer Token: " + token);
+            config.bearer = token;
+        }
+    }
+};
+module.exports = {
+    get: {
+        bearer: function() {
+            var enc_secret = new Buffer(config.consumer_key + ':' + config.consumer_secret).toString('base64');
+            var post_data = qs.stringify({
+                'grant_type': 'client_credentials'
+            });
+            var request = https.request({
+                host: 'api.twitter.com',
+                path: '/oauth2/token',
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Basic ' + enc_secret,
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                    'Content-Length': Buffer.byteLength(post_data)
+                }
+            }, function(response) {
+                var data = '';
+                var error = '';
+                response.on("data", function(d) {
+                    data += d;
+                });
+                response.on("error", function(e) {
+                    error = e;
+                });
+                response.on('end', function() {
+                    if (error) {
+                        throw error;
+                    } else {
+                        var token = JSON.parse(data);
+                        callback.set.bearer(token.access_token);
+                    }
+                });
+            });
+            request.on("error", function(e) {
+                console.log("Error retrieving the Twitter API Bearer token!");
+                console.log(JSON.stringify(e));
+            });
+            request.write(post_data);
+            request.end();
+        }
+    }
+};
